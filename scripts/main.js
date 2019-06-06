dabeec2f9648e0cd757dd8db4313b05c8052d747
Wladpm <wladimir.pielea-melincu@hetic.net>
2019-06-06 15:08:45 +0200

    video adding

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-$(document).foundation()
+const lecteur = document.getElementById('lecteur')
+const buttonmute = document.getElementById('buttonmutesound')
+const iconMute = document.getElementById('iconMute')
+
+buttonmute.addEventListener('click', () => {
+    if (lecteur.paused) {
+        lecteur.play()
+        iconMute.src = "./images/on.png"
+    } else {
+        lecteur.pause()
+        iconMute.src = "./images/mute.png"
+    }
+})
